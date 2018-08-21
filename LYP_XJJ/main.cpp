49d3329e41daa010a029c95adb78e015e9b0a8ca
lypxhxjj <792512795@qq.com>
2018-08-22 00:16:20 +0800

    Initial Commit

---
+++ LYP_XJJ/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  LYP_XJJ
+//
+//  Created by lyp on 2018/8/22.
+//  Copyright © 2018年 lyp. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
